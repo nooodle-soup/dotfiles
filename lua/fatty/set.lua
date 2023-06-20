dfccb52b798245db99f7b74b5b0517bcec300b9c
vineet-agarwa11 <vineetagarwal2402@gmail.com>
2023-06-19 19:25:25 -0700

    Modified set.lua

--- lua/fatty/set.lua
+++ lua/fatty/set.lua
@@ -1,31 +1,34 @@
-vim.opt.guicursor = ""
+local set = vim.opt
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+set.guicursor = ""
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+set.nu = true
+set.relativenumber = true
 
-vim.opt.smartindent = true
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
 
-vim.opt.wrap = false
+set.smartindent = true
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+set.wrap = false
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+set.swapfile = false
+set.backup = false
+set.undodir = os.getenv("HOME") .. "/.vim/undodir"
+set.undofile = true
 
-vim.opt.termguicolors = true
+set.hlsearch = false
+set.incsearch = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+set.termguicolors = true
 
-vim.opt.updatetime = 50
+set.scrolloff = 8
+set.signcolumn = "yes"
+set.isfname:append("@-@")
 
-vim.opt.colorcolumn = "80"
+set.updatetime = 50
+
+set.colorcolumn = "80"
+
