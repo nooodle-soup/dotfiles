42edf9e0d128531ffc7db347ff765599968670fd
vineet-agarwa11 <vineetagarwal2402@gmail.com>
2023-06-20 14:13:40 -0700

    Revert "Modified set.lua"
    This reverts commit dfccb52b798245db99f7b74b5b0517bcec300b9c.

--- lua/fatty/set.lua
+++ lua/fatty/set.lua
@@ -1,34 +1,31 @@
-local set = vim.opt
+vim.opt.guicursor = ""
 
-set.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
-set.nu = true
-set.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
-set.expandtab = true
+vim.opt.smartindent = true
 
-set.smartindent = true
+vim.opt.wrap = false
 
-set.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-set.swapfile = false
-set.backup = false
-set.undodir = os.getenv("HOME") .. "/.vim/undodir"
-set.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
-set.hlsearch = false
-set.incsearch = true
+vim.opt.termguicolors = true
 
-set.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
-set.scrolloff = 8
-set.signcolumn = "yes"
-set.isfname:append("@-@")
+vim.opt.updatetime = 50
 
-set.updatetime = 50
-
-set.colorcolumn = "80"
-
+vim.opt.colorcolumn = "80"
