4acd660d9981119bfa2ce75d2fac95ce9984fe09
vineet-agarwa11 <vineetagarwal2402@gmail.com>
2023-06-21 09:47:42 -0700

    Fixed slow insert issue

--- lua/fatty/packer.lua
+++ lua/fatty/packer.lua
@@ -21,30 +21,11 @@ return require('packer').startup(function(use)
 	  end
   })
 
-  use({
-      "folke/trouble.nvim",
-      config = function()
-          require("trouble").setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
-  })
-
-  use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = function()
-		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-		  ts_update()
-	  end,}
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use("nvim-treesitter/playground")
   use("theprimeagen/harpoon")
-  use("theprimeagen/refactoring.nvim")
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
-  use("nvim-treesitter/nvim-treesitter-context");
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
@@ -68,11 +49,6 @@ return require('packer').startup(function(use)
 		  {'rafamadriz/friendly-snippets'},
 	  }
   }
-
-  use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
-  use("eandrju/cellular-automaton.nvim")
-  use("laytan/cloak.nvim")
 
 end)
 
